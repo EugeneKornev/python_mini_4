dc550be504d6e374aa356a71579b296c8a820bd6
EugeneKornev <yevgene.kornev@gmail.com>
2024-10-11 08:46:16 +0700

    Solution3

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ def dict_to_dict(d: dict) -> dict:
     new_d = {}
     for k, v in d.items():
         if new_d.get(v, None):
-            new_d[v] = tuple({new_d[v], k})
+            if not isinstance(new_d[v], tuple):
+                new_d[v] = (new_d[v], k)
+            else:
+                new_d[v] += (k, )
         else:
             new_d[v] = k
     return new_d
@@ -10,4 +13,5 @@ def dict_to_dict(d: dict) -> dict:
 
 if __name__ == "__main__":
     assert dict_to_dict({"Ivanov": 97832, "Petrov": 55521, "Kuznecov": 97832}) == {97832: ("Ivanov", "Kuznecov"), 55521: "Petrov"}
+    assert dict_to_dict({"Ivanov": 97832, "Petrov": 97832, "Kuznecov": 97832}) == {97832: ("Ivanov", "Petrov", "Kuznecov")}
     assert dict_to_dict({"Ivanov": 97832, "Petrov": 55521, "Kuznecov": [97832, 55521]})
